fd9a868bebbb9fd1c12caecae61d2c58afb82d66
Lucas neves <69198104+webMasterAlumni@users.noreply.github.com>
2023-08-15 19:48:42 -0300

    adicionado percistencia nos dados

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,17 @@ import "./index.css";
 import { router } from "./router.tsx";
 import { Provider } from "react-redux";
 import { store } from "./store/main.store.ts";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+
+const persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<RouterProvider router={router} />
+			<PersistGate loading={null} persistor={persistor}>
+				<RouterProvider router={router} />
+			</PersistGate>
 		</Provider>
 	</React.StrictMode>
 );
--- src/store/main.store.ts
+++ src/store/main.store.ts
@@ -1,11 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { useSelector, TypedUseSelectorHook, useDispatch } from "react-redux";
 import { userSlice } from "./reducer/user.reducer";
+import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+	key: "root",
+	storage: storage,
+};
+
+const reducers = combineReducers({
+	user: userSlice.reducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 export const store = configureStore({
-	reducer: {
-		user: userSlice.reducer,
-	},
+	reducer: persistedReducer,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
